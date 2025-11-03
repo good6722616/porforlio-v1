69b27d012ffd4e86513af1c3d6520b1ed4542d56
Charles Zhang <shhwkk54@gmail.com>
2025-11-03 01:25:36 -0800

    fix: update page title and meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,11 @@ export default function Document() {
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="author" content="Codeefly" />
+        <meta name="author" content="Charles Zhang" />
+        <meta
+          name="description"
+          content="Charles Zhang - Software Developer Portfolio. Full-stack developer specializing in React.js, Next.js, Node.js, and cloud technologies."
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
@@ -23,7 +27,7 @@ export default function Document() {
         <link rel="stylesheet" href="assets/css/plugins.css" />
         <link rel="stylesheet" href="assets/css/custom.css" />
         <link rel="stylesheet" href="assets/css/main.css" />
-        <title>Tokyo</title>
+        <title>Charles Zhang - Software Developer Portfolio</title>
       </Head>
       <body>
         <Main />
